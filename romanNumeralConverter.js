69f91468e018f57ad6712bc9cb05a940cfae71bf
snowyPigeon <snoiwypigeon@yahoo.com>
2022-02-17 15:31:25 +0000

    File created and set up with instructions

---
+++ romanNumeralConverter.js
@@ -0,0 +1,16 @@
+/*
+# Free Code Camp, JavaScript Algorithms and Data Structures
+# 
+# Roman Numeral Converter
+# 
+# Convert the given number into a roman numeral.
+#
+# All roman numerals answers should be provided in upper-case.
+#
+*/
+
+function convertToRoman(num) {
+    return num;
+   }
+   
+   convertToRoman(36);
