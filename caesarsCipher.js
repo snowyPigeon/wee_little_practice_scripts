ef2b8f64b512c309890982b832cdfd5f88ed79ba
snowyPigeon <snoiwypigeon@yahoo.com>
2022-02-20 14:15:44 +0000

    Began solution, helper decode function and rot13 function partially complete with plain English instructions

--- caesarsCipher.js
+++ caesarsCipher.js
@@ -3,22 +3,65 @@
 # 
 # Caesars Cipher
 #
-# One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
-# 
+# One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. 
+# In a shift cipher the meanings of the letters are shifted by some set amount.
+#
 # A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
 # 
 # Write a function which takes a ROT13 encoded string as input and returns a decoded string.
 # 
 # All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
 #
-# See https://www.mathsisfun.com/roman-numerals.html for the symbols.
 */
+const cipher = new Map([
+  ["A", "N"],
+  ["B", "O"],
+  ["C", "P"],
+  ["D", "Q"],
+  ["E", "R"],
+  ["F", "S"],
+  ["G", "T"],
+  ["H", "U"],
+  ["I", "V"],
+  ["J", "W"],
+  ["K", "X"],
+  ["L", "Y"],
+  ["M", "Z"],
+  ["N", "A"],
+  ["O", "B"],
+  ["P", "C"],
+  ["Q", "D"],
+  ["R", "E"],
+  ["S", "F"],
+  ["T", "G"],
+  ["U", "H"],
+  ["V", "I"],
+  ["W", "J"],
+  ["X", "K"],
+  ["Y", "L"],
+  ["Z", "M"],
+]);
+
+function decode(value, array) {
+  var current = cipher.get(value);
+  console.log(array);
+  if (current != "") {
+      console.log(current);
+    value = current;
+    current = "";
+    // replace inputArray[value] with decoded value
+  }
+}
 
 function rot13(str) {
-    return str;
-  }
-  
-  rot13("SERR PBQR PNZC");
+  const inputArray = str.split("");
+  console.log(inputArray);
+  inputArray.forEach(decode);
+  // convert inputArray back into a string.
+  return str;
+}
+
+rot13("SERR PBQR PNZC");
 
 /*
 # Tests
